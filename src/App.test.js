bf5bed56ab11f2b284228198cdc1044dfc069680
Jon Oliver <imjonoliver@gmail.com>
2017-05-24 22:41:21 -0400

    Mock Request in spec

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Request from './Request';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  Request.default = jest.fn();
   ReactDOM.render(<App />, div);
 });
