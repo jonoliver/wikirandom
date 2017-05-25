8193063a915dff99ec8fb857aa06814fd2a80d4d
Jon Oliver <imjonoliver@gmail.com>
2017-05-25 01:04:45 -0400

    :white_check_mark: test: App contains Article

--- src/App/index.test.js
+++ src/App/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
+import { mount } from 'enzyme'
 import * as Request from '../Request';
 
 const div = document.createElement('div');
@@ -13,4 +14,12 @@ it('renders without crashing', () => {
 it('makes the initial wiki request', () => {
   ReactDOM.render(<App />, div);
   expect(Request.default).toBeCalled();
+});
+
+it('contains an Article', () => {
+  const article = { title: 'A title' }
+  const element = mount(<App />)
+  element.setState({article: article})
+  expect(element.find('article').length).toBe(1);
+  expect(element.find('article h1').text()).toBe('A title');
 });
