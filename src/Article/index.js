dd9973992f8190379fcd874fa2e190c0d02083fd
Jon Oliver <imjonoliver@gmail.com>
2017-05-26 02:03:27 -0400

    :smirk_cat: Fall back to 'en' image if no 'commons' exists

--- src/Article/index.js
+++ src/Article/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { Component } from 'react';
 import './Article.css';
 
-const Article = ({article}) => {
-  const { title, url, body, image } = article;
-  return (
-    <article>
-      <h1><a href={url}>{title}</a></h1>
-      <p>{body}</p>
-      { image &&
-        <img src={image} alt={title} onError={(e) => e.target.style.display='none'} />
-      }
-    </article>
-  )
+class Article extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      article: props.article,
+      backupImgAttempted: false
+    };
+    this.swapSrc = this.swapSrc.bind(this);
+  }
+
+  swapSrc(e) {
+    if (this.state.backupImgAttempted){
+      e.target.style.display = 'none';
+    }
+    else {
+      const src = e.target.src;
+      e.target.src = src.replace('/commons/', '/en/');
+      this.setState({backupImgAttempted: true});
+    }
+  }
+
+  render(){
+    const { title, url, body, image } = this.state.article;
+    return(
+      <article>
+        <h1><a href={url}>{title}</a></h1>
+        <p>{body}</p>
+        { image &&
+          <img src={image} alt={title} onError={this.swapSrc} />
+        }
+      </article>
+    )
+  }
 }
 
 export default Article;
